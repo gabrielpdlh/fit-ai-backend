0324c2b6b54ccc45fc70ae2312f519814d9c6f7c
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2026-05-05 21:36:00 -0300

    chore(zod): add zod and fastify-type-provider-zod setup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,42 @@
 import "dotenv/config";
 
 import Fastify from "fastify";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { z } from "zod/v4";
 
-const fastify = Fastify({
+const app = Fastify({
   logger: true,
 });
 
-fastify.get("/", async function handler() {
-  return { hello: "world" };
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/",
+  schema: {
+    description: "Returns Hello World",
+    tags: ["Hello World"],
+    response: {
+      200: z.object({
+        message: z.string(),
+      }),
+    },
+  },
+  handler: () => {
+    return {
+      message: "Hello World",
+    };
+  },
 });
 
 try {
-  await fastify.listen({ port: Number(process.env.PORT) || 3000 });
+  await app.listen({ port: Number(process.env.PORT) || 3000 });
 } catch (err) {
-  fastify.log.error(err);
+  app.log.error(err);
   process.exit(1);
 }
